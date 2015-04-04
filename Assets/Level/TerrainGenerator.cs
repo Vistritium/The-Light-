c3f4ff62738aa74e1c78f0540f00c27a0a63716d
kubek777 <pktryq@gmail.com>
2015-04-04 20:23:54 +0200

    Spawning 4 obstacles

--- Assets/Level/TerrainGenerator.cs
+++ Assets/Level/TerrainGenerator.cs
@@ -33,7 +33,20 @@ namespace Assets
 					Debug.Log("adding new tile");
 					var hazard = Instantiate (terrain);
 					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = UnityEngine.Random.Range(-3, 3) * Vector3.right;
+					hazard.transform.localPosition = UnityEngine.Random.Range(-4, 4) * Vector3.right;
+
+				var hazard1 = Instantiate (terrain);
+				hazard1.transform.parent = newTile.transform;
+				hazard1.transform.localPosition = UnityEngine.Random.Range(-4, 4) * Vector3.left;
+
+				var hazard2 = Instantiate (terrain);
+				hazard2.transform.parent = newTile.transform;
+				hazard2.transform.localPosition = UnityEngine.Random.Range(-4, 4) * Vector3.right;
+				
+				var hazard3 = Instantiate (terrain);
+				hazard3.transform.parent = newTile.transform;
+				hazard3.transform.localPosition = UnityEngine.Random.Range(-4, 4) * Vector3.left;
+
 				    var wall = Instantiate (this.wall);
 			    	wall.transform.parent = newTile.transform;
 					wall.transform.localPosition = UnityEngine.Random.Range(1, 1)  * Vector3.up + UnityEngine.Random.Range(5, 5) * Vector3.left;
