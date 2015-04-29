e5953a8d8a9661eb7717de8c6e91a89c4e9aca48
Patryk <pktryq@gmail.com>
2015-04-29 11:48:24 +0200

    przeszkody +

--- Assets/Level/TerrainGenerator.cs
+++ Assets/Level/TerrainGenerator.cs
@@ -14,6 +14,7 @@ namespace Assets
 		public GameObject pickUp;
 		System.Random rnd = new System.Random();
 	public int n;
+		public int p;
 		// Use this for initialization
 		private void Start()
 		{
@@ -34,112 +35,215 @@ namespace Assets
 			foreach (var newTile in newTiles) {
 				var wall = Instantiate (this.wall);
 				wall.transform.parent = newTile.transform;
-				wall.transform.localPosition = Vector3.up + UnityEngine.Random.Range (5, 5) * Vector3.left;
+				wall.transform.localPosition = Vector3.up + UnityEngine.Random.Range (6, 6) * Vector3.left;
 				
 				var wall1 = Instantiate (this.wall);
 				wall1.transform.parent = newTile.transform;
-				wall1.transform.localPosition = Vector3.up + UnityEngine.Random.Range (5, 5) * Vector3.right;
-
-				var pickup1 = Instantiate (pickUp);
-				pickup1.transform.parent = newTile.transform;
-				pickup1.transform.localPosition = UnityEngine.Random.Range (-3, 3) * Vector3.right 
-					+ UnityEngine.Random.Range (1, 1) * Vector3.up
-						+ UnityEngine.Random.Range (3, 3) * Vector3.forward ;
-
+				wall1.transform.localPosition = Vector3.up + UnityEngine.Random.Range (6, 6) * Vector3.right;
+			
 			}
 			// Spawnowanie przeszkod
 			
 			foreach (var newTile in newTiles) {
 
-					 n = rnd.Next (0, 5);	
+					 n = rnd.Next (0,7 );	
 				if (n == 0) {
-					
+					// *     *
+					// *     *
+					//**_____**
+					  if (p!=2) {
+					//lewa strona
 					var hazard = Instantiate (terrain);
 					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = UnityEngine.Random.Range (-4, -4) * Vector3.right;
+					hazard.transform.localPosition = -4 * Vector3.right
+							+ 0.5f*Vector3.up;
 
 					var hazard1 = Instantiate (terrain);
 					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = UnityEngine.Random.Range (-3, -3) * Vector3.right;
+						hazard1.transform.localPosition = -3 * Vector3.right + 0.5f*Vector3.up;
 
 					var hazard2 = Instantiate (terrain);
 					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = UnityEngine.Random.Range (-4, -4) * Vector3.right 
-						+ UnityEngine.Random.Range (1, 1) * Vector3.up;
+					hazard2.transform.localPosition = -4* Vector3.right 
+							+  1 * Vector3.forward + 0.5f*Vector3.up;
 
-						
+					var hazard3 = Instantiate (terrain);
+					hazard3.transform.parent = newTile.transform;
+						hazard3.transform.localPosition = -3 * Vector3.right 
+							+ 1 * Vector3.forward + 0.5f*Vector3.up;
+
+					var hazard4 = Instantiate (terrain);
+					hazard4.transform.parent = newTile.transform;
+					hazard4.transform.localPosition = -3 * Vector3.right 
+							+ 2 * Vector3.forward + 0.5f*Vector3.up;
+					// prawa strona
+					var hazard5 = Instantiate (terrain);
+					hazard5.transform.parent = newTile.transform;
+						hazard5.transform.localPosition = 4 * Vector3.right + 0.5f*Vector3.up;
+					
+					var hazard6 = Instantiate (terrain);
+					hazard6.transform.parent = newTile.transform;
+						hazard6.transform.localPosition = 3 * Vector3.right + 0.5f*Vector3.up;
+					
+					var hazard7 = Instantiate (terrain);
+					hazard7.transform.parent = newTile.transform;
+					hazard7.transform.localPosition = 3 * Vector3.right 
+							+ 1* Vector3.forward + 0.5f*Vector3.up;
+					p = 0;
+					}
+					else continue;
+
 				} else if (n == 1) {
-					
+
+					//        *  
+					//      ***
+					//______***  
+					//prawa strona
 					var hazard1 = Instantiate (terrain);
 					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = UnityEngine.Random.Range (4, 4) * Vector3.right;
+					hazard1.transform.localPosition = 4 * Vector3.right + 0.5f*Vector3.up;
 
 					var hazard = Instantiate (terrain);
 					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = UnityEngine.Random.Range (3, 3) * Vector3.right;
+					hazard.transform.localPosition =  3 * Vector3.right + 0.5f*Vector3.up;
 
 					var hazard2 = Instantiate (terrain);
 					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = UnityEngine.Random.Range (4, 4) * Vector3.right 
-						+ UnityEngine.Random.Range (1, 1) * Vector3.up;
+					hazard2.transform.localPosition = 4 * Vector3.right 
+						+ 1 * Vector3.forward + 0.5f*Vector3.up;
 
+					var hazard3 = Instantiate (terrain);
+					hazard3.transform.parent = newTile.transform;
+					hazard3.transform.localPosition = 3 * Vector3.right 
+						+ 1 * Vector3.forward + 0.5f*Vector3.up;
+
+					var hazard4 = Instantiate (terrain);
+					hazard4.transform.parent = newTile.transform;
+					hazard4.transform.localPosition = UnityEngine.Random.Range (3, 3) * Vector3.right 
+						+ 2 * Vector3.forward + 0.5f*Vector3.up;
+					//lewa strona
+					/*
+					var hazard6 = Instantiate (terrain);
+					hazard6.transform.parent = newTile.transform;
+					hazard6.transform.localPosition = UnityEngine.Random.Range (1, 1) * Vector3.left; 
+
+					var hazard5 = Instantiate (terrain);
+					hazard5.transform.parent = newTile.transform;
+					hazard5.transform.localPosition = UnityEngine.Random.Range (1, 1) * Vector3.left 
+						+ UnityEngine.Random.Range (1, 1) * Vector3.forward;
+						*/
+					p = 1;
 				}
 				else if (n == 2) {
-					
+					//
+					// ___*
+					// __**
+					// __*
+
 					var hazard1 = Instantiate (terrain);
 					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = UnityEngine.Random.Range (0, 0) * Vector3.right
-						+ UnityEngine.Random.Range (2, 2) * Vector3.forward ;
+					hazard1.transform.localPosition =  0 * Vector3.left + 0.5f*Vector3.up;
+						
 
 
 					var hazard2 = Instantiate (terrain);
 					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = UnityEngine.Random.Range (0, 0) * Vector3.right
-						+ UnityEngine.Random.Range (1, 1) * Vector3.up
-							+ UnityEngine.Random.Range (2, 2) * Vector3.forward ;
+					hazard2.transform.localPosition = 1 * Vector3.left + 0.5f*Vector3.up;
 
+					var hazard3 = Instantiate (terrain);
+					hazard3.transform.parent = newTile.transform;
+					hazard3.transform.localPosition = 0 * Vector3.left
+						+ 1* Vector3.forward  + 0.5f*Vector3.up;
+
+					var hazard4 = Instantiate (terrain);
+					hazard4.transform.parent = newTile.transform;
+					hazard4.transform.localPosition = 1 * Vector3.left
+						+ 1* Vector3.back  + 0.5f*Vector3.up;
+					
+					p = 2;
 				}
 
 				else if (n == 3) {
-					
+					//
+					//    *
+					//*___*______
 					var hazard1 = Instantiate (terrain);
 					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = UnityEngine.Random.Range (4, 4) * Vector3.right;
+					hazard1.transform.localPosition =  4 * Vector3.left + 0.5f*Vector3.up;
 					
 					var hazard = Instantiate (terrain);
 					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = UnityEngine.Random.Range (3, 3) * Vector3.right;
+					hazard.transform.localPosition =   1 * Vector3.right + 0.5f*Vector3.up;
 					
 					var hazard2 = Instantiate (terrain);
 					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = UnityEngine.Random.Range (3, 3) * Vector3.right 
-						+ UnityEngine.Random.Range (1, 1) * Vector3.up;
-					
+					hazard2.transform.localPosition = 1 * Vector3.right 
+						+ 1 * Vector3.forward + 0.5f*Vector3.up;
+					p = 3;
 				}
 
 				else if (n == 4) {
-					
+				
+					continue;
+
+				}
+
+				else if (n == 5) {
+					//
+					//
+					//____****____
 					var hazard1 = Instantiate (terrain);
 					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = UnityEngine.Random.Range (-4, -4) * Vector3.right;
+					hazard1.transform.localPosition = 0 * Vector3.left + 0.5f*Vector3.up;
 					
 					var hazard = Instantiate (terrain);
 					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = UnityEngine.Random.Range (-3, -3) * Vector3.right;
+					hazard.transform.localPosition =  0 * Vector3.right + 0.5f*Vector3.up;
 					
 					var hazard2 = Instantiate (terrain);
 					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = UnityEngine.Random.Range (-3, -3) * Vector3.right 
-						+ UnityEngine.Random.Range (1, 1) * Vector3.up;
+					hazard2.transform.localPosition =  1 * Vector3.right + 0.5f*Vector3.up;
+
+					var hazard3 = Instantiate (terrain);
+					hazard3.transform.parent = newTile.transform;
+					hazard3.transform.localPosition =  1 * Vector3.right + 0.5f*Vector3.up;
+
+					p = 5;
+				
+				
+				}
+
+			else if (n == 6) {
+					//
+					//*
+					//**
+					//**
+
+					var hazard1 = Instantiate (terrain);
+					hazard1.transform.parent = newTile.transform;
+					hazard1.transform.localPosition =  4 * Vector3.left + 0.5f*Vector3.up;
 					
+					var hazard = Instantiate (terrain);
+					hazard.transform.parent = newTile.transform;
+					hazard.transform.localPosition =  3 * Vector3.left + 0.5f*Vector3.up;
+					
+					var hazard2 = Instantiate (terrain);
+					hazard2.transform.parent = newTile.transform;
+					hazard2.transform.localPosition = 4 * Vector3.left 
+						+ 1 * Vector3.forward + 0.5f*Vector3.up;
+					
+					var hazard3 = Instantiate (terrain);
+					hazard3.transform.parent = newTile.transform;
+					hazard3.transform.localPosition = 3 * Vector3.left 
+						+ 1 * Vector3.forward + 0.5f*Vector3.up;
+					
+					var hazard4 = Instantiate (terrain);
+					hazard4.transform.parent = newTile.transform;
+					hazard4.transform.localPosition = 3 * Vector3.left 
+						+ 2 * Vector3.forward + 0.5f*Vector3.up;
+				
 				}
-				/*
-			else if (n == 5) {
-				var jumper1 = Instantiate (jumper);
-				jumper1.transform.parent = newTile.transform;
-				jumper1.transform.localPosition = UnityEngine.Random.Range (0, 0) * Vector3.right;
-				}
-				 */
+				 
 				
 			}
 			
