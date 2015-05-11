7ae29b1405488653ad350ec3f0a84cc5b86d15bc
K56 <krzysiek56000@gmail.com>
2015-05-11 18:24:50 +0200

    + Terrain generator  - with generating audi rings and dashpads

--- Assets/Level/TerrainGenerator.cs
+++ Assets/Level/TerrainGenerator.cs
@@ -12,10 +12,15 @@ namespace Assets
 		public GameObject wall;
 		public GameObject jumper;
 		public GameObject pickUp;
+		public GameObject dashPad;
+		public GameObject audiRing;
 		System.Random rnd = new System.Random();
-	public int n;
+		public int n;
 		public int p;
-
+		
+		private CameraTargetScript cameraScript;
+		
+		/*
 		//ustawienia przeszkod
 		public int x01;
 		public float y01;
@@ -23,42 +28,77 @@ namespace Assets
 		public int x02;
 		public float y02;
 		public int z02;
-		public int x03;
-		public float y03;
-		public int z03;
-		public int x04;
-		public float y04;
-		public int z04;
-		public int x05;
-		public float y05;
-		public int z05;
-
 		public int x11;
-		public float y11;
+		public int y11;
 		public int z11;
-		public int x12;
-		public float y12;
-		public int z12;
-		public int x13;
-		public float y13;
-		public int z13;
-
 		public int x21;
-		public float y21;
+		public int y21;
 		public int z21;
-		public int x22;
-		public float y22;
-		public int z22;
-		public int x23;
-		public float y23;
-		public int z23;
-
-
+		public int x31;
+		public int y31;
+		public int z31;
+		*/
+		
+		public float[,,] smallObstacles;
+		public int[] smallObstacleNumbers;
+		public int smallObstaclesAmount;
+		
+		public float path = 1;
+		public float pathMin = 1;
+		public float pathMax = 7;
+		public float pathSpeed = 0;
+		public float pathAcceleration = 2;
+		public float pathMaxTurnSpeed = 0.5f;
+		public float pathWidth = 3;
+		public int pathTimerMin = 5;
+		public int pathTimerMax = 10;
+		
+		public int obstacleDistMin = 6;
+		public int obstacleDistMax = 15;
+		public float obstacleDistSpeedMultiplier = 2;
+		public float obstacleDistSpeedBase = 2;
+		public float obstacleDistBase = 10;
+		public int obstacleFreeTime = 10;
+		
+		public int ringDistanceMin = 100;
+		public int ringDistanceMax = 150;
+		public int ringCounter = 300;
+		
+		public int dashPadDistanceMin = 50;
+		public int dashPadDistanceMax = 200;
+		public int dashPadCounter = 500;
+		
+		public int obstacleCounter = 50;
+		
+		private int pathDirection = 0;
+		private float pathTimer = 5;
+		
+		private float[] paths;
+		public int[] pathsCounters;
+		
 		// Use this for initialization
 		private void Start()
 		{
 			var tileGenerator = GetComponent<TileGenerator>();
 			tileGenerator.newTilesAdded += NewTilesAdded;
+			
+			paths = new float[] {1, 4, 7};
+			pathsCounters = new int[] {1, 1, 1};
+			
+			smallObstacles = new float[,,] 
+			{ 
+				{ {-1, 0, -1}, {0, 0, -1},  {-1, 0, 0}, {0, 0, 0},  {1, 0, 0}, {0, 0, 1},  {1, 0, 1} },
+				{ {-1, 0, 1}, {-1, 0, 0},  {0, 0, 0}, {1, 0, 0},  {1, 0, -1}, {0, 0, 0},  {0, 0, 0} },
+				{ {-1, 0, 0}, {0, 0, 0},  {1, 0, 0}, {1, 0, 1},  {1, 0, -1}, {0, 0, 0},  {0, 0, 0} }
+			};
+			
+			smallObstacleNumbers = new int[]
+			{7, 5, 5};
+			
+			smallObstaclesAmount = 3;
+			
+			cameraScript = GameObject.Find ("Player").GetComponent<CameraTargetScript>();
+			
 		}
 		
 		// Update is called once per frame
@@ -70,7 +110,8 @@ namespace Assets
 		
 		//called when new tiles are generated
 		private void NewTilesAdded(List<GameObject> newTiles)
-		{ 		
+		{ 	
+			// Spawn side walls:
 			foreach (var newTile in newTiles) {
 				var wall = Instantiate (this.wall);
 				wall.transform.parent = newTile.transform;
@@ -79,172 +120,265 @@ namespace Assets
 				var wall1 = Instantiate (this.wall);
 				wall1.transform.parent = newTile.transform;
 				wall1.transform.localPosition = Vector3.up + UnityEngine.Random.Range (6, 6) * Vector3.right;
-			
-			}
-			// Spawnowanie przeszkod
-			
-			foreach (var newTile in newTiles) {
-
-					 n = rnd.Next (0,3);	
-				if (n == 0) {
-
-					var hazard = Instantiate (terrain);
-					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = x01 * Vector3.right
-							+ y01*Vector3.up + z01*Vector3.forward;
-
-					var hazard1 = Instantiate (terrain);
-					hazard1.transform.parent = newTile.transform;
-						hazard1.transform.localPosition = x02 * Vector3.right + y02*Vector3.up + z02*Vector3.forward;
-
-						var hazard2 = Instantiate (terrain);
-						hazard2.transform.parent = newTile.transform;
-						hazard2.transform.localPosition = x03 * Vector3.right + y03*Vector3.up + z03*Vector3.forward;
-					/*
-						var hazard3 = Instantiate (terrain);
-						hazard3.transform.parent = newTile.transform;
-						hazard3.transform.localPosition = x04 * Vector3.right + y04*Vector3.up + z04*Vector3.forward;
-
-						
-						var hazard5 = Instantiate (terrain);
-						hazard5.transform.parent = newTile.transform;
-						hazard5.transform.localPosition = x05 * Vector3.right + y05*Vector3.up + z05*Vector3.forward;
-*/
-					p=0;
-					} 
-						else if (n == 1) {
-					var hazard = Instantiate (terrain);
-					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = x11 * Vector3.right
-						+ y11*Vector3.up + z11*Vector3.forward;
-					
-					var hazard1 = Instantiate (terrain);
-					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = x12 * Vector3.right + y12*Vector3.up + z12*Vector3.forward;
-					
-					var hazard2 = Instantiate (terrain);
-					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = x13 * Vector3.right + y13*Vector3.up + z13*Vector3.forward;
-					/*
-						var hazard3 = Instantiate (terrain);
-						hazard3.transform.parent = newTile.transform;
-						hazard3.transform.localPosition = x14 * Vector3.right + y14*Vector3.up + z14*Vector3.forward;
-
-						
-						var hazard5 = Instantiate (terrain);
-						hazard5.transform.parent = newTile.transform;
-						hazard5.transform.localPosition = x15 * Vector3.right + y15*Vector3.up + z15*Vector3.forward;
-*/
-					p=1;
-				}
-
-				else if (n == 2) {
-					//
-					// ___*
-					// __**
-					// __*
-
-					var hazard = Instantiate (terrain);
-					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition = x21 * Vector3.right
-						+ y21*Vector3.up + z21*Vector3.forward;
-					
-					var hazard1 = Instantiate (terrain);
-					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = x22 * Vector3.right + y22*Vector3.up + z22*Vector3.forward;
-					
-					var hazard2 = Instantiate (terrain);
-					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = x23 * Vector3.right + y23*Vector3.up + z23*Vector3.forward;
-
-					
-					p = 2;
-				}
-
-				else if (n == 3) {
-					//
-					//    *
-					//*___*______
-					var hazard1 = Instantiate (terrain);
-					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition =  4 * Vector3.left + 0.5f*Vector3.up;
-					
-					var hazard = Instantiate (terrain);
-					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition =   1 * Vector3.right + 0.5f*Vector3.up;
-					
-					var hazard2 = Instantiate (terrain);
-					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = 1 * Vector3.right 
-						+ 1 * Vector3.forward + 0.5f*Vector3.up;
-					p = 3;
-				}
-
-				else if (n == 4) {
-				
-					continue;
-
-				}
-
-				else if (n == 5) {
-					//
-					//
-					//____****____
-					var hazard1 = Instantiate (terrain);
-					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition = 0 * Vector3.left + 0.5f*Vector3.up;
-					
-					var hazard = Instantiate (terrain);
-					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition =  0 * Vector3.right + 0.5f*Vector3.up;
-					
-					var hazard2 = Instantiate (terrain);
-					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition =  1 * Vector3.right + 0.5f*Vector3.up;
-
-					var hazard3 = Instantiate (terrain);
-					hazard3.transform.parent = newTile.transform;
-					hazard3.transform.localPosition =  1 * Vector3.right + 0.5f*Vector3.up;
-
-					p = 5;
-				
-				
-				}
-
-			else if (n == 6) {
-					//
-					//*
-					//**
-					//**
-
-					var hazard1 = Instantiate (terrain);
-					hazard1.transform.parent = newTile.transform;
-					hazard1.transform.localPosition =  4 * Vector3.left + 0.5f*Vector3.up;
-					
-					var hazard = Instantiate (terrain);
-					hazard.transform.parent = newTile.transform;
-					hazard.transform.localPosition =  3 * Vector3.left + 0.5f*Vector3.up;
-					
-					var hazard2 = Instantiate (terrain);
-					hazard2.transform.parent = newTile.transform;
-					hazard2.transform.localPosition = 4 * Vector3.left 
-						+ 1 * Vector3.forward + 0.5f*Vector3.up;
-					
-					var hazard3 = Instantiate (terrain);
-					hazard3.transform.parent = newTile.transform;
-					hazard3.transform.localPosition = 3 * Vector3.left 
-						+ 1 * Vector3.forward + 0.5f*Vector3.up;
-					
-					var hazard4 = Instantiate (terrain);
-					hazard4.transform.parent = newTile.transform;
-					hazard4.transform.localPosition = 3 * Vector3.left 
-						+ 2 * Vector3.forward + 0.5f*Vector3.up;
-				
-				}
-				 
 				
 			}
 			
+			
+			foreach (var newTile in newTiles)
+			{
+				// Blocks next to walls
+				
+				// Path
+				
+				// Enemies
+				
+				// Pickups
+				
+				// DashPads
+				
+				
+				for (int i = 0; i < 10; i++)
+					CalculationStep(newTile, i);
+				
+			}
+			
+		}
+		
+		private void CalculationStep (GameObject tile, int step)
+		{
+			int i, j;
+			
+			// Calculate actual obstacleDistance:
+			obstacleFreeTime = (int)((cameraScript.speed + cameraScript.speedAdditional + obstacleDistSpeedBase) * obstacleDistSpeedMultiplier + obstacleDistBase);
+			
+			// Decrement counters:
+			obstacleCounter--;
+			
+			if (ringCounter > 0)
+				ringCounter--;
+			
+			if (dashPadCounter > 0)
+				dashPadCounter--;
+			
+			for (i = 0; i < 3; i++)
+			{
+				if (pathsCounters[i] > 0)
+					pathsCounters[i]++;
+			}
+			
+			#region Set obstacles[]' values, where new obstacles should be
+			
+			// If it is time to place an obstacle, do it:
+			if (obstacleCounter == 0)
+			{
+				// Firstly, reset the counter:
+				obstacleCounter = UnityEngine.Random.Range(obstacleDistMin, obstacleDistMax);
+				
+				bool[] obstacles = new bool[] {false, false, false};
+				bool done = false;
+				
+				// Sum up, how many driving routes are there:
+				int sum = 0;
+				
+				for (i = 0; i < 3; i++)
+					if (pathsCounters[i] > obstacleFreeTime)
+						sum++;
+				
+				// If exactly two paths are free, occupy one of them:
+				if (sum == 2)
+					for (i = 0; i < 2; i++)
+				{
+					if (pathsCounters[i] > obstacleFreeTime && pathsCounters[i+1] > obstacleFreeTime)
+					{
+						obstacles[i + UnityEngine.Random.Range(0, 2)] = true;
+						done = true;
+						
+						for (j = 0; j < 3; j++)
+						{
+							if (pathsCounters[j] <= obstacleFreeTime)
+								obstacles[j] = true;
+						}
+					}
+				}
+				
+				// Else, if two paths on the sides are free, do nothing .:
+				if (done == false && pathsCounters[0] > obstacleFreeTime && pathsCounters[2] > obstacleFreeTime && pathsCounters[1]  <= obstacleFreeTime)
+				{
+					//if (UnityEngine.Random.Range(0, 2) == 1)
+					//	obstacles[1] = true;
+					
+					done = true;
+				}
+				
+				// If you have one pathway, that player had to take, create one obstacle around it:
+				if (done == false && sum == 1)
+				{
+					j = UnityEngine.Random.Range(0, 3 - sum);
+					i = 0;
+					while (done == false && i < 3)
+					{
+						if (pathsCounters[i] <= obstacleFreeTime)
+						{
+							if (j == 0)
+							{
+								//sum--;
+								obstacles[i] = true;
+								done = true;
+							}
+							else
+								j--;
+						}
+						
+						i++;
+					}
+					
+					done = true;
+				}
+				
+				// If you have free field, create one or two obstacles:
+				if (done == false)
+				{
+					if (UnityEngine.Random.Range(0, 2) == 1)
+					{
+						// Place one obstacle:
+						obstacles[UnityEngine.Random.Range(0, 3)] = true;
+						
+						done = true;
+					}
+					else
+					{
+						// Place two obstacles:
+						
+						i = UnityEngine.Random.Range(0, 3);
+						
+						if (i <2 && pathsCounters[1] < obstacleFreeTime * 2)
+							i = 2;
+						
+						if (i == 0 && pathsCounters[0] < obstacleFreeTime * 2)
+							i++;
+						
+						if (i == 1 && pathsCounters[2] < obstacleFreeTime * 2)
+							i++;
+						
+						obstacles[i] = true;
+						if (i < 2)
+							obstacles[i + 1] = true;
+						else
+							obstacles[0] = true;
+						
+						done = true;
+					}
+				}
+				
+				#endregion
+				
+				
+				// For each one of chosen lanes, create a wall there:
+				for (i = 0; i < 3; i++)
+				{
+					if (obstacles[i] == true)
+					{
+						CreateSmallObstacle(tile, new Vector3(paths[i] - 4f, 0.5f, step - 4.5f));
+						
+						//pathsCounters[i] = 8;
+					}
+				}
+				
+				// Place pickups:
+				if (ringCounter == 0)
+				{
+					if (PlaceObject(obstacles, tile, audiRing, step, 1.5f) == 1)
+						ringCounter = UnityEngine.Random.Range(ringDistanceMin, ringDistanceMax);
+				}
+				else if (dashPadCounter == 0)
+				{
+					if (PlaceObject(obstacles, tile, dashPad, step, 0f) == 1)
+						dashPadCounter = UnityEngine.Random.Range(dashPadDistanceMin, dashPadDistanceMax);
+				}
+				
+				// Now, mark the possible driving routes for future iterations:
+				for (i = 0; i < 3; i++)
+				{
+					if (obstacles[i] == false)
+					{
+						//pathsCounters[i] = 1;
+					}
+					else
+					{
+						pathsCounters[i] = 1;
+					}
+				}
+			}
+			
+			/*
+			// Create obstacles:
+			if (step == 3 || step == 7)
+			{
+				//CreateBlock(tile, new Vector3(path - 4f, 2.5f, step - 4.5f));
+			}
+			*/
+		}
+		
+		
+		private void CreateSmallObstacle(GameObject tile, Vector3 pos)
+		{
+			int chosen = UnityEngine.Random.Range (0, smallObstaclesAmount);
+			
+			for (int i = 0; i < smallObstacleNumbers[chosen]; i++)
+			{
+				CreateBlock(tile, new Vector3(pos.x + smallObstacles[chosen, i, 0], pos.y + smallObstacles[chosen, i, 1], pos.z + smallObstacles[chosen, i, 2]), terrain);
+			}
+		}
+		
+		private void CreateBlock(GameObject tile, Vector3 pos, GameObject obj)
+		{
+			if (pos.x % 1 != 0)
+			{
+				pos.x = Mathf.Round(pos.x);
+			}
+			
+			var wall = Instantiate (obj);
+			wall.transform.parent = tile.transform;
+			wall.transform.localPosition = pos;
+		}
+		
+		private int PlaceObject(bool[] obstacles, GameObject tile, GameObject obj, int step, float height)
+		{
+			int sum = 0;
+			int j = 3;
+			int i;
+			for (i = 0; i < 3; i++)
+			{
+				if (obstacles[i] == false && pathsCounters[i] > 0)
+				{
+					sum++;
+				}
+				else if (sum > 1)
+				{
+					j = i;
+					i = 3;
+				}
+				else
+				{
+					sum = 0;
+				}
+			}
+			
+			if (sum > 1)
+			{
+				if (UnityEngine.Random.Range(0, 2) == 1)
+				{
+					CreateBlock(tile, new Vector3(paths[j - 1] - 4f, height, step - 4.5f), obj);
+				}
+				else
+				{
+					CreateBlock(tile, new Vector3(paths[j - sum] - 4f, height, step - 4.5f), obj);
+				}
+				
+				return 1;
+			}
+			
+			return 0;
 		}
 	}
 }
